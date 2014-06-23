f48335fe0421f5e4955d7a6eb603cd895f22c621
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-06-23 14:31:34 +0200

    add logout button to app

--- www/app/login/login.ctrl.js
+++ www/app/login/login.ctrl.js
@@ -1,9 +1,14 @@
 angular.module('login.ctrl', [])
 
-.controller ('LoginCtrl', function ($scope) {
+.controller ('LoginCtrl', function ($scope, $state) {
 
   $scope.login = function (credentials) {
     console.log(credentials);
-  }
+
+    $state.go('despachosMenu.start');
+
+  };
+
+
 
 });
--- www/app/ticket/ticket.ctrl.js
+++ www/app/ticket/ticket.ctrl.js
@@ -4,15 +4,8 @@ angular.module('ticket.ctrl', [])
 
   .controller('TicketCtrl', function ($scope, $state) {
 
-    // get all relevant data from MySQL and populate
-
-    // Ticket.getAll();
-
-    // get: Truck ->
-    // get: Driver ->
-    // get: Weight ->
-    // get: Photos ->
-    // get: Batches ->
-    // get: ??????
-
+    $scope.logout = function () {
+      console.log('logged out now from ticket');
+      $state.go('login');
+    }
   });
